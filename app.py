2cd7bf9404653171429c5b34e139c283492b0025
nia <niaaabelezinha@gmail.com>
2025-04-28 18:09:05 -0300

    sabrinacarpenter

---
+++ app.py
@@ -0,0 +1,8 @@
+num1 = int(input("digite um numero: "))
+num2= int(input("digite o segundo número: "))
+
+if (num1 % 2 == 0 and num2 % 2 == 0):
+    print("são pares")
+    
+else:
+    print("são impares")
